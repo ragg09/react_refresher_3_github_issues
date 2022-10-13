5be29fe9cd8b559471445737ddcc0f49845659ba
ragg09 <ragunayon@gmail.com>
2022-10-13 17:11:27 +0800

    Git REST API | React Markdown

--- src/Details.jsx
+++ src/Details.jsx
@@ -1,68 +1,62 @@
 import React from 'react'
+import { useQuery } from 'react-query';
+import { useParams } from 'react-router-dom';
+import { formatDistance } from 'date-fns'
+import Comments from './Comments';
+import ReactMarkdown from 'react-markdown';
 
 export default function Details() {
 
-    const fakeArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    
+    const params = useParams();
+
+    const {
+		isLoading, 
+		isSuccess,
+		data: issue,
+	} = useQuery(['issue', params.id], fetchIssue); //by adding 2nd param, it will force refetch
+
+	function fetchIssue() {
+		return fetch(`https://api.github.com/repos/facebook/create-react-app/issues/${params.id}`).then(response => response.json());
+	}
 
   return (
         <div className="comments-container">
-            <h2>
-            [DevTools] Improve named hooks detection <span>#21782</span>
-            </h2>
-            <div className="issue-details">
-            <a href="">bvaughn</a> opened this issue 6 days ago
-            </div>
+            {isLoading && <div>Loading . . .</div>}
 
-            <div className="comment-container">
-            <a href="#">
-                <img
-                src="https://avatars.githubusercontent.com/u/69965670?s=88&v=4"
-                alt="avatar"
-                className="avatar"
-                />
-            </a>
-            <div className="comment">
-                <div className="comment-heading">
-                <a href="#">mdaj06</a> commented 4 days ago
-                </div>
-                <div className="comment-body">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum
-                sint optio et sit nemo expedita excepturi corrupti delectus? Unde
-                nemo eos quo, similique minima, maiores perspiciatis deserunt,
-                eligendi eum consequuntur vero quam non laboriosam illum ipsam ex
-                pariatur voluptatum. Cumque itaque dolores nostrum optio
-                perspiciatis quibusdam voluptatibus animi tempore labore.
-                </div>
-            </div>
-            </div>
+            {isSuccess && (
+                <>
+                    <h2>
+                    {issue.title} <span>#{issue.number}</span>
+                    </h2>
+                    <div className="issue-details">
+                    <a href={issue.user.html_url}>{issue.user.login}</a> opened this issue {formatDistance(new Date(issue.created_at) , new Date(), { addSuffix: true })}
+                    </div>
 
-            <div className="border"></div>
+                    <div className="comment-container">
+                    <a href={issue.user.html_url}>
+                        <img
+                        src={issue.user.avatar_url}
+                        alt="avatar"
+                        className="avatar"
+                        />
+                    </a>
+                    <div className="comment">
+                        <div className="comment-heading">
+                        <a href={issue.user.html_url}>{issue.user.login}</a> commented {formatDistance(new Date(issue.created_at) , new Date(), { addSuffix: true })}
+                        </div>
+                        <div className="comment-body markdown-body">
+                            <ReactMarkdown children={issue.body}/>                            
+                        </div>
+                    </div>
+                    </div>
 
-            {fakeArray.map(item => (
-            <div key={item} className="comment-container">
-                <a href="#">
-                <img
-                    src="https://avatars.githubusercontent.com/u/69965670?s=88&v=4"
-                    alt="avatar"
-                    className="avatar"
-                />
-                </a>
-                <div className="comment">
-                <div className="comment-heading">
-                    <a href="#">mdaj06</a> commented 4 days ago
-                </div>
-                <div className="comment-body">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum
-                    sint optio et sit nemo expedita excepturi corrupti delectus?
-                    Unde nemo eos quo, similique minima, maiores perspiciatis
-                    deserunt, eligendi eum consequuntur vero quam non laboriosam
-                    illum ipsam ex pariatur voluptatum. Cumque itaque dolores
-                    nostrum optio perspiciatis quibusdam voluptatibus animi tempore
-                    labore.
-                </div>
-                </div>
-            </div>
-            ))}
+                    <div className="border border-2"></div>
+
+                    <Comments issueNumber={issue.number}/>
+                </>
+            )}
+            
         </div>
   )
 }
--- src/Issues.jsx
+++ src/Issues.jsx
@@ -5,7 +5,8 @@ import { useQuery } from 'react-query';
 import IconOpen from './IconOpen';
 import IconClose from './IconClose';
 import { useState } from 'react';
-import { formatDistance, subDays } from 'date-fns'
+import { formatDistance} from 'date-fns'
+
 
 
 function App() {
@@ -58,7 +59,7 @@ function App() {
 		{isSuccess && (
 			<div className="issues-container">
 				<div className="issues-heading">
-				<a href="#">facebook / create-react-app</a>
+				<a href="https://github.com/facebook/create-react-app">facebook / create-react-app</a>
 				<div className="open-closed-buttons">
 					<button
 						onClick={() => setFilter('open')}
@@ -89,7 +90,7 @@ function App() {
 						{issue.state === 'open' && <IconOpen/>}
 						{issue.state === 'closed' && <IconClose/>}
 						<div className="issues-title">
-							<Link to={`/issues/1`}>
+							<Link to={`/issues/${issue.number}`}>
 								{issue.title}
 							</Link>
 						<div className="issues-title-details">
@@ -99,7 +100,7 @@ function App() {
 					</div>
 
 					{issue.comments > 0 && (
-						<Link to={`/issues/1`} className="comments-count-container">
+						<Link to={`/issues/${issue.number}`} className="comments-count-container">
 							<svg
 							className="octicon octicon-comment v-align-middle"
 							viewBox="0 0 16 16"
@@ -109,7 +110,7 @@ function App() {
 							aria-hidden="true"
 							>
 							<path
-								fill-rule="evenodd"
+								fillRule="evenodd"
 								d="M2.75 2.5a.25.25 0 00-.25.25v7.5c0 .138.112.25.25.25h2a.75.75 0 01.75.75v2.19l2.72-2.72a.75.75 0 01.53-.22h4.5a.25.25 0 00.25-.25v-7.5a.25.25 0 00-.25-.25H2.75zM1 2.75C1 1.784 1.784 1 2.75 1h10.5c.966 0 1.75.784 1.75 1.75v7.5A1.75 1.75 0 0113.25 12H9.06l-2.573 2.573A1.457 1.457 0 014 13.543V12H2.75A1.75 1.75 0 011 10.25v-7.5z"
 							></path>
 							</svg>
